5413266160afc36183eb2962594490815c169085
zhulx <zhulx521@gmail.com>
2024-10-25 11:08:28 +0800

    增加SelectNumber重置事件

--- packages/fusion-ui/src/components/select-number/select-number.tsx
+++ packages/fusion-ui/src/components/select-number/select-number.tsx
@@ -17,23 +17,26 @@ const SelectNumber : React.FC<SelectNumberProps> = function SelectNumber({
     const [selectValue, setSelectValue] = useState(value?.substring(0, 1));
     const [numberValue, setNumberValue] = useState(value?.substring(1));
 
-    // useEffect(() => {
-    //     if (value) {
-    //         setSelectValue(value.substring(0, 1));
-    //         if (value.length > 1) {
-    //             setNumberValue(value.substring(1));
-    //         }
-    //     }
-    //   }, [value]);
+    useEffect(() => {
+        if (value) {
+            // setSelectValue(value.substring(0, 1));
+            // if (value.length > 1) {
+            //     setNumberValue(value.substring(1));
+            // }
+        } else {
+          setSelectValue(null);
+          setNumberValue(null);
+        }
+      }, [value]);
   
     const handleSelectChange = (val : any) => {
       setSelectValue(val);
-      onChange(`${val}${numberValue || ''}`);
+      onChange(`${val || ''}${numberValue || ''}`);
     };
   
     const handleNumberChange = (val : any) => {
       setNumberValue(val);
-      onChange(`${selectValue || ''}${val}`);
+      onChange(`${selectValue || ''}${val || ''}`);
     };
   
     return (<>
